312b917aff81438bdc829313d738e33b74b78ecb
Sem van Nieuwenhuizen <leukenaamofzo@gmail.com>
2024-01-11 18:13:22 +0100

    feat: Change build output to `/file.html` instead of `/file/index.html`

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,8 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  // To ensure that our provider displays `/partners` without trailing slashes output as files instead of directories
+  build: { format: 'file' },
   integrations: [
     icon({ include: { mdi: ['*'] } }),
     sitemap(),
