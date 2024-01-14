6c621297c88e91ae1151147993c51708176dcbe8
Sem van Nieuwenhuizen <leukenaamofzo@gmail.com>
2024-01-14 18:06:32 +0100

    feat: Use the latest entry by sorting on `id` instead of `date`, this simplifies resolving the latest entry

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { getCollection } from 'astro:content'
-
-const entries = await getCollection('editions')
-
-// The latest edition is the one with the most recent date
-export const [latest] = entries
-  .sort((a, b) => a.data.date.getTime() - b.data.date.getTime())
-  .slice(-1)
